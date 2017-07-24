810149b1f60025b5942b5c694803bf31114af473
Mike Tomothy Q <mike@simplesimple.ca>
2017-07-24 14:52:16 -0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var minify = require('gulp-babel-minify');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+  js: 'js/src/**',
+  scss: 'scss/**',
+  images: '/images/src/**'
+};
+
+gulp.task('default', function () {
+  gulp.watch(paths.scss, gulp.parallel('scss'));
+  gulp.watch(paths.js, gulp.parallel('minify_js'));
+  gulp.watch(paths.images, gulp.parallel('minify_images'));
+});
+
+gulp.task('minify_js', function () {
+  return gulp.src('js/src/*.js')
+    .pipe(minify({
+      ext: {
+        src: '.js',
+        min: '.js'
+      }
+    })).pipe(gulp.dest('js/build/'))
+});
+
+gulp.task('scss', function () {
+  return gulp.src('scss/styles.scss')
+    .pipe(sourcemaps.init({
+      loadMaps: true,
+      identifyMap: true
+    }))
+    .pipe(sass())
+    .pipe(sourcemaps.write('.', {
+      sourceRoot: 'css'
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('minify_images', function () {
+  return gulp.src('images/src/**')
+    .pipe(imagemin())
+    .pipe(gulp.dest('images/'));
+});
